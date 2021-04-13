3a96559c615f4289eec2a1b19c3277a0660608de
parksangwoo <b01056721617@gmail.com>
2021-04-13 23:09:37 +0900

    open source

--- 3-2-types/index.js
+++ 3-2-types/index.js
@@ -2,12 +2,14 @@ const { ApolloServer } = require('apollo-server')
 
 const queries = require('./typedefs-resolvers/_queries')
 const mutations = require('./typedefs-resolvers/_mutations')
+const enums = require('./typedefs-resolvers/_enums')
 const equipments = require('./typedefs-resolvers/equipments')
 const supplies = require('./typedefs-resolvers/supplies')
 
 const typeDefs = [
     queries,
     mutations,
+    enums,
     equipments.typeDefs,
     supplies.typeDefs
 ]
